a40186326b2ed38315d62bab9c37e92873fc2571
rakhi singhal <rakhisinghal@zapbuild.com>
2025-01-22 17:01:13 +0530

    Done the push notification by firebase and notifee with message body and image

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Navigation from './src/navigation/Navigation'
+import Pushnotification from './src/Components/Pushnotification'
 
 const App = () => {
   return (
-    <Navigation/>
+    <>
+     <Navigation/>
+  
+    </>
+   
   )
 }
 
--- index.js
+++ index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';  // Import Provider from react-redux
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import store from './src/Components/redux/store';
 
-AppRegistry.registerComponent(appName, () => App);
+// Wrap your App component with Provider and pass the Redux store
+AppRegistry.registerComponent(appName, () => () => (
+  <Provider store={store}>  {/* Redux Provider wraps your App */}
+    <App />
+  </Provider>
+));
